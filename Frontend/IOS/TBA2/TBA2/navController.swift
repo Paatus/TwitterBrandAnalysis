f3b953da737ee8a4a5afdbd2038aa623d1d8286a
Jessie-SEM <gusxioxi@student.gu.se>
2014-12-04 13:35:34 +0100

    side menu to select views
    Used a library ENSideMenu

---
+++ Frontend/IOS/TBA2/TBA2/navController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  sideMenuTest
+//
+//  Created by Xiaoqian Xiong on 03/12/14.
+//  Copyright (c) 2014 Xiaoqian Xiong. All rights reserved.
+//
+
+import UIKit
+
+class navController: ENSideMenuNavigationController, ENSideMenuDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sideMenu = ENSideMenu(sourceView: self.view, menuTableViewController: menuTable(), menuPosition:.Left)
+        sideMenu?.delegate = self //optional
+        sideMenu?.menuWidth = 180.0 // optional, default is 160
+        //sideMenu?.bouncingEnabled = false
+        
+        // make navigation bar showing over side menu
+        view.bringSubviewToFront(navigationBar)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - ENSideMenu Delegate
+    func sideMenuWillOpen() {
+        println("sideMenuWillOpen")
+    }
+    
+    func sideMenuWillClose() {
+        println("sideMenuWillClose")
+    }
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
+
