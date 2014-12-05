60ffb569c5caab7920bd8ad7149f596ec2ed6f07
twistedretard <kenny@kento.se>
2014-12-05 07:17:05 +0100

    Small error

--- backend_web/src/backend_user.erl
+++ backend_web/src/backend_user.erl
@@ -14,12 +14,12 @@ get_user_keywords(Req) ->
 add_user_keywords(Req, Key) ->
     Username = backend_login:get_username(Req),
     Keywords = lists:append(get_user_keywords(Req), Key),
-    backend_db:put(?ACCOUNT_BUCKET, Username, Keywords).
+    backend_db:put(?ACCOUNT_BUCKET, Username, Keywords, []).
 
 del_user_keywords(Req, Key) ->
     Username = backend_login:get_username(Req),
     Keywords = lists:delete(Key, get_user_keywords(Req)),
-    backend_db:put(?ACCOUNT_BUCKET, Username, Keywords).
+    backend_db:put(?ACCOUNT_BUCKET, Username, Keywords, []).
 
 create_account(Username, Password) ->
     backend_db:put(?LOGIN_BUCKET, Username, backend_utils:hash_input(Password),[]),
