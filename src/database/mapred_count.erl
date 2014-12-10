d292506113e66db3e05c53b698d9b3885cf12f32
Ian Jenkins <ianjenkinssem@gmail.com>
2014-12-10 16:04:34 +0100

    mapreduce of pos/net/neg tweets of keyword- Brand History Track

--- src/database/mapred_count.erl
+++ src/database/mapred_count.erl
@@ -10,13 +10,27 @@ mapred_count(Keys) ->
 		{reduce, {modfun, ?MODULE, red_count}, none, true}]
         ),
         riakio:close_link(Pid),
-        {ok, R}.
+        {ok, dict:to_list(R)}.
 	%Output of this should be {ok, [{positive, X}, {neutral, Y}, {negative, Z}]}
 
 map_count(RiakObject, _, _) ->
-	{{text,T},{weight,W},{timezone,TZ}} = binary_to_term(riak_object:get_value(RiakObject)),
-	[result_of_map].
+	{_,{weight,W},_} = binary_to_term(riak_object:get_value(RiakObject)),
+	[dict:from_list([
+		case W of
+			1 ->
+				positive;
+			0.5 ->
+				neutral;
+			0 ->
+				negative
+		end, 1])].
+	
 
 red_count(Input, _) ->
-	[result_of_reduce].
+	[lists:foldl(fun(W, Acc)->
+					dict:merge(fun(_,X,Y)-> X+Y end,
+						W, Acc)
+				end,
+				dict:new(),
+				Input)].
 
