8fbf173b8624c2ad0ba75654b9a00a9da8c3e898
Shan Jiang <gusjiash@gmail.com>
2014-12-17 02:32:20 +0800

    add spinner when loading data for website

--- Frontend/web/js/main.js
+++ Frontend/web/js/main.js
@@ -1,6 +1,28 @@
-
-
 $(function() {
+    var opts = {
+      lines: 11, // The number of lines to draw
+      length: 40, // The length of each line
+      width: 12, // The line thickness
+      radius: 49, // The radius of the inner circle
+      corners: 1, // Corner roundness (0..1)
+      rotate: 17, // The rotation offset
+      direction: 1, // 1: clockwise, -1: counterclockwise
+      color: '#000', // #rgb or #rrggbb or array of colors
+      speed: 0.8, // Rounds per second
+      trail: 56, // Afterglow percentage
+      shadow: false, // Whether to render a shadow
+      hwaccel: true, // Whether to use hardware acceleration
+      className: 'spinner', // The CSS class to assign to the spinner
+      zIndex: 2e9, // The z-index (defaults to 2000000000)
+      top: '50%', // Top position relative to parent
+      left: '50%' // Left position relative to parent
+    };
+    var target = document.getElementById('container');
+    var spinner = new Spinner(opts).spin(target);
+
+
+
+
 // Public Page
     // login function
     $('#login-name').keyup(function(){
