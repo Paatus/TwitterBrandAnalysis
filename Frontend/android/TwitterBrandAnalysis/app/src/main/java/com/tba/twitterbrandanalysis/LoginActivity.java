4211cd7f934770f7c8ffbd65e5d35e5ac03c1cd2
Pontus Hjortskog <p.hjortskog@gmail.com>
2014-12-16 14:59:51 +0100

    this should fix the login, and also project stuff for android

--- Frontend/android/TwitterBrandAnalysis/app/src/main/java/com/tba/twitterbrandanalysis/LoginActivity.java
+++ Frontend/android/TwitterBrandAnalysis/app/src/main/java/com/tba/twitterbrandanalysis/LoginActivity.java
@@ -3,8 +3,24 @@ package com.tba.twitterbrandanalysis;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.jar.Attributes;
 
 public class LoginActivity extends Activity {
 
@@ -22,8 +38,24 @@ public class LoginActivity extends Activity {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(LoginActivity.this, MainActivity.class);
-                i.putExtra("User", "TestUser");
-                startActivity(i);
+                TextView user_text = (TextView) findViewById(R.id.username);
+                TextView pass_text = (TextView) findViewById(R.id.password);
+                try {
+                    HttpClient client = new DefaultHttpClient();
+                    HttpPost post = new HttpPost("http://dev.kento.se:8080/api/login");
+                    List<NameValuePair> credentials = new ArrayList<NameValuePair>(2);
+                    credentials.add(new BasicNameValuePair("username", user_text.getText().toString()));
+                    credentials.add(new BasicNameValuePair("pwd", pass_text.getText().toString()));
+                    post.setEntity(new UrlEncodedFormEntity(credentials));
+
+                    HttpResponse response = client.execute(post);
+                    String resp_str = new BasicResponseHandler().handleResponse(response);
+                    Toast.makeText(LoginActivity.this, resp_str, Toast.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    Log.e("TBA ERROR", "exception in login" + e.getClass().getName() + " - - " + e.getMessage());
+                }
+                //i.putExtra("User", "TestUser");
+                //startActivity(i);
             }
         });
     }
