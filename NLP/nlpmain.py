7492bf2f25444102bb1a0d896f4ba7c3ff3ab09a
twistedretard <kenny@kento.se>
2015-01-08 11:54:02 +0100

    Fixed some bugs and improved performance some.

--- NLP/nlpmain.py
+++ NLP/nlpmain.py
@@ -1,70 +1,56 @@
-import nltk
 from nltk import *
 import pickle
 import re
 import string
 import json
 
-#start 
-def processTweet(tweet):
-    #decode bytestring utf-8
-    #tweet = tweet.decode('utf-8')
-    #Convert to lower case
-    tweet = tweet.lower()
-    #Convert www.* or https?://* to URL
-    tweet = re.sub('((www\.[\s]+)|(https?://[^\s]+))','URL',tweet)
-    #Convert @username to AT_USER
-    tweet = re.sub('@[^\s]+','AT_USER',tweet)
-    #Remove additional white spaces
-    tweet = re.sub('[\s]+', ' ', tweet)
-    #Replace #word with word
-    tweet = re.sub(r'#([^\s]+)', r'\1', tweet)
-    #look for 3 or more repetitions of character and replace with the character itself "happyyyy" = "happy"
-    tweet = re.sub(r'(.)\1\1\1+', r'\1', tweet)
-    #trim
-    tweet = tweet.strip('\'"')
-    #remove punctuation 
-    tweet = re.sub('[%s]' % re.escape(string.punctuation), '', tweet)    
-    return tweet
-#end
+stopwords_data = None
+wordlist = None
+model = None
 
-#start
+def processTweet(tweet):
+    tweet = tweet.lower()
+    tweet = re.sub(r'((www\.[\s]+)|(https?://[^\s]+))','URL',tweet)
+    tweet = re.sub(r'[!"#$%&\'()*+,\-_./:;<=>?\\\[\]^`\{\|\}~]', '', tweet)    
+    tweet = re.sub(r'@\S+','AT_USER',tweet)
+    tweet = re.sub(r'(\S)\1{2,}', r'\1\1', tweet)
+    tweet = re.sub(r'\s{2,}', ' ', tweet)
+    tweet = re.sub(r'(^\s+|\s+$)', '', tweet)
+    return tweet
+
 def stopwords(tweet):
+    global stopwords_data
     tweet = processTweet(tweet)
     tokens = []
-    tweet = nltk.word_tokenize(tweet)
-    stopwords = pickle.load(open("NLP/stopwords.p", "rb"))
-    
-    '''Append list y elimaitaing StopWords in tweet - StopWords e.g. [I,am,the,of...]'''
+    tweet = word_tokenize(tweet)
+    if not stopwords_data:
+        with open("stopwords.p", "rb") as f:
+            stopwords_data = pickle.load(f)
     for word in tweet:
-        if word not in stopwords:
+        if word not in stopwords_data:
             tokens.append(word)
     return tokens
-#end
 
-#start
 def extract_features(document):
-    wordlist = pickle.load(open("NLP/wordlist.p", "rb"))
+    global wordlist
+    if not wordlist:
+        with open("wordlist.p", "rb") as f:
+            wordlist = pickle.load(f)
     document_words = set(document)
     features = {}
     for word in wordlist:
         features['contains(%s)' % word] = (word in document_words)
-        
-        #print("word features = ", word)
-        #e_features = pickle.dump(features, open("e_features.p", "wb"))
-    #print("features = ",features)   
     return features
-#end
 
-#start
 def getWeight(tweet):
-    #features = pickle.load(open("features.p", "rb"))
+    global model
     tweet = tweet.decode('utf-8')
     tweet = processTweet(tweet)
-    model = pickle.load(open("NLP/model.p", "rb"))
-    '''call processTweet function'''
+    print(tweet)
+    if not model:
+        with open("model.p", "rb") as f:
+            model = pickle.load(f)
     tweet = stopwords(tweet)
     document = tweet
     weight = model.classify(extract_features(document))
     return weight
-#end    
